35f295d8f0aaf6af837ce9018ea7ae87e8d0c724
art2cat <yiming.whz@gmail.com>
2017-01-20 14:14:30 +0800

    update

--- AnimationArt/app/src/main/java/com/art2cat/dev/animationart/MainActivity.java
+++ AnimationArt/app/src/main/java/com/art2cat/dev/animationart/MainActivity.java
@@ -1,13 +1,30 @@
 package com.art2cat.dev.animationart;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+
+import drawable.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 确保VectorDrawable 向下兼容使用于Button控件，还要配合selector xml配置文件
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.activity_main);
+        if (fragment == null) {
+            fragmentManager.beginTransaction()
+                    .add(R.id.activity_main, new MainFragment())
+                    .commit();
+        }
     }
 }
---
+++ AnimationArt/app/src/main/java/drawable/MainFragment.java
@@ -0,0 +1,36 @@
+package drawable;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.art2cat.dev.animationart.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+}
